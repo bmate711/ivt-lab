fa128f8ca751cc3d7dd5ac7ab71996db29c1eb92
Bodor Máté <bodor.mate@kszk.bme.hu>
2019-04-05 09:23:03 +0200

    change

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -79,7 +79,7 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
-         
+          int alma = 1;
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
             firingSuccess = secondaryTorpedoStore.fire(1);
